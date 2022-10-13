80edd4d047091a73394a6104829ccef27a41e158
JesusRueda1 <jdrueda83@misena.edu.co>
2022-10-13 10:23:05 -0500

    Base de datos actualizada, e intento de validar sesion

--- BASE DE DATOS/tic.sql
+++ BASE DE DATOS/tic.sql
@@ -12,7 +12,7 @@ MySQL - 10.4.24-MariaDB : Database - tic
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`tic` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`tic` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci */;
 
 USE `tic`;
 
@@ -24,10 +24,11 @@ CREATE TABLE `articulo` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_persona` int(11) NOT NULL,
   `articulo` varchar(50) COLLATE utf32_spanish_ci NOT NULL,
-  `datos_entrada` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `datos_salida` timestamp NULL DEFAULT NULL,
+  `datos_entrada` time NOT NULL,
+  `datos_salida` time DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `id_persona` (`id_persona`)
+  KEY `id_persona` (`id_persona`),
+  CONSTRAINT `articulo_ibfk_1` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_spanish_ci;
 
 /*Data for the table `articulo` */
@@ -42,14 +43,17 @@ CREATE TABLE `persona` (
   `nombre` varchar(50) COLLATE utf32_spanish_ci NOT NULL,
   `apellido` varchar(50) COLLATE utf32_spanish_ci NOT NULL,
   `correo` varchar(50) COLLATE utf32_spanish_ci NOT NULL,
-  `contraseña` varchar(50) COLLATE utf32_spanish_ci NOT NULL,
+  `contraseña` varchar(150) COLLATE utf32_spanish_ci NOT NULL,
   `rol` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `rol` (`rol`),
-  CONSTRAINT `persona_ibfk_1` FOREIGN KEY (`id`) REFERENCES `articulo` (`id_persona`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_spanish_ci;
+  CONSTRAINT `persona_ibfk_2` FOREIGN KEY (`rol`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf32 COLLATE=utf32_spanish_ci;
 
 /*Data for the table `persona` */
+
+insert  into `persona`(`id`,`documento`,`nombre`,`apellido`,`correo`,`contraseña`,`rol`) values 
+(1,1048065338,'Jesus David','Rueda Correa','jdrueda83@misena.edu.co','$2y$10$HC6jDpyH/.DoiKr5uH7Xsu2GDkmDB65bAUX3Y45xpDW85FjkGg6we',1);
 
 /*Table structure for table `roles` */
 
@@ -58,11 +62,16 @@ DROP TABLE IF EXISTS `roles`;
 CREATE TABLE `roles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `descripcion` varchar(50) COLLATE utf32_spanish_ci NOT NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `roles_ibfk_1` FOREIGN KEY (`id`) REFERENCES `persona` (`rol`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_spanish_ci;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf32 COLLATE=utf32_spanish_ci;
 
 /*Data for the table `roles` */
+
+insert  into `roles`(`id`,`descripcion`) values 
+(1,'administrador'),
+(2,'vigilante'),
+(3,'instructor'),
+(4,'aprendiz');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
---
+++ php/validar_sesion.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+
+$login = $_SESSION['login'];
+if (isset($login)== true){
+//codigo normal
+}
+?>
