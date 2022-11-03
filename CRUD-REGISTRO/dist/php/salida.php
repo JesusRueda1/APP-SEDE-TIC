192ada0403f491b32d4b3ec3611a667bab2ba9f0
JesusRueda1 <jdrueda83@misena.edu.co>
2022-11-03 11:42:19 -0500

    salida

--- CRUD-REGISTRO/dist/php/salida.php
+++ CRUD-REGISTRO/dist/php/salida.php
@@ -3,7 +3,9 @@ include('../../../conexion/conexion.php');
 $id_articulo = $_POST['id_articulos'];
 session_start();
 /* $id_articulo = $_SESSION['id_articulo']; */
-
+echo"<script>
+        alert('el id de los articulos ingresados en salida es: ',$id_articulo);
+    </script>";
 include('datetime.php');
 $SQL = "UPDATE articulo SET datos_salida='".$hora."' WHERE id='".$id_articulo."';";
 $query = mysqli_query($conexion,$SQL);
@@ -12,7 +14,7 @@ if ($query) {
         <script>
             alert('Hora de salida Actualizada!');
             location.href='../DASHBOARD.php';
-        </>
+        </script>
     ";
 }
 
