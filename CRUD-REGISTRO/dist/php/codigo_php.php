eb0b55479543a2ddb0d53247af087fd08f32e200
JesusRueda1 <jdrueda83@misena.edu.co>
2022-11-29 11:12:01 -0500

    ultimas pinceladas

--- CRUD-REGISTRO/dist/php/codigo_php.php
+++ CRUD-REGISTRO/dist/php/codigo_php.php
@@ -24,13 +24,13 @@ if($query6){
         <div class="caja">
             <div class="row">
                 <div class="col-md-3">
-                    <label for="nom">Nombre</label> <input type="text" id="nom" name="nom" placeholder="Escriba aqui" required class="form-control" value="<?php echo $nombre?>">
+                    <label for="nom">Nombre</label> <input type="text" id="nom" name="nom" placeholder="Escriba aqui" required class="form-control"  value="<?php echo $nombre?>" readonly>
                 </div>
                 <div class="col-md-3">
-                    <label for="ape">Apellido</label> <input type="text" id="ape" name="ape" placeholder="Escriba aqui" required class="form-control" value="<?php echo $apellido?>">
+                    <label for="ape">Apellido</label> <input type="text" id="ape" name="ape" placeholder="Escriba aqui" required class="form-control" value="<?php echo $apellido?>" readonly>
                 </div>
                 <div class="col-md-3">
-                    <label for="doc">Documento</label> <input type="text" id="doc" name="doc" placeholder="Escriba aqui" required class="form-control " value="<?php echo $documento?>">
+                    <label for="doc">Documento</label> <input type="text" id="doc" name="doc" placeholder="Escriba aqui" required class="form-control " value="<?php echo $documento?>" readonly>
                 </div>
             </div>
             <div class="row">
@@ -39,13 +39,9 @@ if($query6){
                     <?php 
                     $SQL3 = "SELECT * FROM roles";
                     $query3= mysqli_query($conexion,$SQL3);?>
-                    <select name="rol" id="rol" required class="form-control">
-                    <option value="">Selecione una opcion...</option>
-                    <option selected value="<?php echo $id_rol; ?>"><?php echo $rol; ?></option>
-                    <?php while($row = mysqli_fetch_array($query3)){?>
-                        <option value="<?php echo $row['id']; ?>"><?php  echo $row['descripcion']; ?></option>
-                        <?php } ?>
-                        </select>
+                    <select name="rol" id="rol" required class="form-control" readonly>
+                        <option selected value="<?php echo $id_rol; ?>"><?php echo $rol; ?></option>
+                    </select>
                 </div>
                 <div class="col-md-3">
                     <label for="articulo">Articulo</label> <input type="text" id="articulo" name="articulo" placeholder="Escriba aqui" required class="form-control">
