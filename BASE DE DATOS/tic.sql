05da73a49312129d4826f1ec1cd69a5f85b2b4f6
JesusRueda1 <jdrueda83@misena.edu.co>
2022-12-04 15:30:53 -0500

    base de datos actualizada

--- BASE DE DATOS/tic.sql
+++ BASE DE DATOS/tic.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Enterprise v13.1.1 (64 bit)
-MySQL - 10.4.25-MariaDB : Database - tic
+MySQL - 10.4.27-MariaDB : Database - tic
 *********************************************************************
 */
 
@@ -23,16 +23,16 @@ DROP TABLE IF EXISTS `articulo`;
 CREATE TABLE `articulo` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_persona` int(11) NOT NULL,
-  `usuario` varchar(80) COLLATE utf32_spanish_ci DEFAULT NULL,
-  `articulo` varchar(50) COLLATE utf32_spanish_ci NOT NULL,
+  `usuario` varchar(80) DEFAULT NULL,
+  `articulo` varchar(50) NOT NULL,
   `fecha` date DEFAULT NULL,
   `datos_entrada` time NOT NULL,
   `datos_salida` time DEFAULT NULL,
-  `observaciones` text COLLATE utf32_spanish_ci DEFAULT NULL,
+  `observaciones` text DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `id_persona` (`id_persona`),
   CONSTRAINT `articulo_ibfk_1` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf32 COLLATE=utf32_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf32 COLLATE=utf32_spanish_ci;
 
 /*Data for the table `articulo` */
 
@@ -43,18 +43,21 @@ DROP TABLE IF EXISTS `persona`;
 CREATE TABLE `persona` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `documento` bigint(20) NOT NULL,
-  `nombre` varchar(50) COLLATE utf32_spanish_ci NOT NULL,
-  `apellido` varchar(50) COLLATE utf32_spanish_ci NOT NULL,
-  `correo` varchar(50) COLLATE utf32_spanish_ci NOT NULL,
-  `contraseña` varchar(150) COLLATE utf32_spanish_ci DEFAULT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `correo` varchar(50) NOT NULL,
+  `contraseña` varchar(150) DEFAULT NULL,
   `rol` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `rol` (`rol`),
   CONSTRAINT `persona_ibfk_2` FOREIGN KEY (`rol`) REFERENCES `roles` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf32 COLLATE=utf32_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf32 COLLATE=utf32_spanish_ci;
 
 /*Data for the table `persona` */
 
+insert  into `persona`(`id`,`documento`,`nombre`,`apellido`,`correo`,`contraseña`,`rol`) values 
+(5,1048065338,'Jesus','Rueda','jdrueda83@misena.edu.co','$2y$10$nGU7gv1y.u1JvZ5WOqlE6.ARuIX2sVKAX0lZUTRvGCv5xfmEiUFsi',1),
+(6,3850232152,'Juanito','Valdés','juanitovaldes@gmail.com','$2y$10$Yy/S/2nWkww62zhAZQO3Du0ksPXtmeW4DmcIcbzNyZDLViKlXkRPy',2);
 
 /*Table structure for table `roles` */
 
@@ -62,9 +65,9 @@ DROP TABLE IF EXISTS `roles`;
 
 CREATE TABLE `roles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `descripcion` varchar(50) COLLATE utf32_spanish_ci NOT NULL,
+  `descripcion` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf32 COLLATE=utf32_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf32 COLLATE=utf32_spanish_ci;
 
 /*Data for the table `roles` */
 
