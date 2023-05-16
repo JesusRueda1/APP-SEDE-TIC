e6c70b4b2b22d62d759d385f6281f910e90a4c1c
RuedaDesarrollador <fesigat@gmail.com>
2023-05-16 15:05:01 -0500

    termine login

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <title>Login</title>
     <meta charset="utf-8">
@@ -23,56 +22,48 @@
         }
     </style>
 </head>
-
 <body class="backgroundFondo">
     <?php // include('msjs.php');?>
-
-        <div class="container"> 
-            <div class="world-latest-articles">
-                <div class="row">
-                    <div class="col-12 col-lg-12">
-                        <div class="form">
-                            <div id="signup">
-                                <h1 class="text-center" style="color: #333; font-weight: bold;">Iniciar Sesión</h1>
-                                <form id="soloNumeros" action="php/ingreso.php" method="post">
-                                    <div class="field-wrap">
-                                        <label>Nro de documento</label>
-                                        <input type="text" name="username" required autocomplete="off">
-                                    </div>
-                                    <div class="field-wrap">
-                                        <label>Contraseña</label>
-                                        <input type="password" name="password" required autocomplete="off"/>
-                                    </div>
-                                    <input type="submit" class="button button-block mb-3 mt-5 miBtn mt-3" value="ENTRAR"/>
-                                    <!-- <a href="#" id="olvidar" title="Recuperar Clave">Recuperar Clave</a> -->
-                                    <br><br>          
-                                </form>
-                            </div>
-                            <div id="recuperarclave">
-                                <h1 class="text-center mb-5 recuperarPass">Recuperar tu Clave</h1>
-                                <form action="#" method="post">
-                                    <div class="field-wrap">
-                                        <label>Nro de Documento</label>
-                                        <input type="number" name="Doc" required autocomplete="off"/>
-                                    </div>
-                                    <input type="submit" class="button button-block miBtn mt-5" value="RECUPERAR CLAVE"/>
-                                    <a href="#" id="volver" class="mt-3 mb-4" title="Volver">Volver</a>
-                                    <br><br>
-                                </form>
-                            </div>
+    <div class="container"> 
+        <div class="world-latest-articles">
+            <div class="row">
+                <div class="col-12 col-lg-12">
+                    <div class="form">
+                        <div id="signup">
+                            <h1 class="text-center" style="color: #333; font-weight: bold;">Iniciar Sesión</h1>
+                            <form id="soloNumeros" action="php/ingreso.php" method="post">
+                                <div class="field-wrap">
+                                    <label>Nro de documento</label>
+                                    <input type="text" name="username" required autocomplete="off">
+                                </div>
+                                <div class="field-wrap">
+                                    <label>Contraseña</label>
+                                    <input type="password" name="password" required autocomplete="off"/>
+                                </div>
+                                <input type="submit" class="button button-block mb-3 mt-5 miBtn mt-3" value="ENTRAR"/>
+                                <!-- <a href="#" id="olvidar" title="Recuperar Clave">Recuperar Clave</a> -->
+                                <br><br>          
+                            </form>
+                        </div>
+                        <div id="recuperarclave">
+                            <h1 class="text-center mb-5 recuperarPass">Recuperar tu Clave</h1>
+                            <form action="#" method="post">
+                                <div class="field-wrap">
+                                    <label>Nro de Documento</label>
+                                    <input type="number" name="Doc" required autocomplete="off"/>
+                                </div>
+                                <input type="submit" class="button button-block miBtn mt-5" value="RECUPERAR CLAVE"/>
+                                <a href="#" id="volver" class="mt-3 mb-4" title="Volver">Volver</a>
+                                <br><br>
+                            </form>
                         </div>
                     </div>
                 </div>
-            </div>   
-        </div>
-
-
-
+            </div>
+        </div>   
+    </div>
 
     <script src="assets/js/jquery-3.3.1.min.js"></script>
     <script src="./assets/js/index.js"></script>
-
-
 </body>
-
 </html>
